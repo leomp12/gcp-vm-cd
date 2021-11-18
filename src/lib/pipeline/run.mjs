cd476a42b2688669ec9b44c68490b6487320b7c0
Leonardo Matos <leomp120894@gmail.com>
2021-11-17 21:56:46 -0300

    chore(run-pipeline): get commands from env or use defaults

--- src/lib/pipeline/run.mjs
+++ src/lib/pipeline/run.mjs
@@ -3,7 +3,14 @@ import pull from './steps/pull';
 import restart from './steps/restart';
 import test from './steps/test';
 
-export default (context) => pull(context)
-  .then(restart)
-  .then(test)
+const {
+  COMMAND_PULL,
+  COMMAND_RESTART,
+  COMMAND_TEST,
+  COMMAND_TEST_OUTPUT,
+} = process.env;
+
+export default (context) => pull(COMMAND_PULL || 'git pull', context)
+  .then(() => restart(COMMAND_RESTART || 'npm run restart', context))
+  .then(() => test(COMMAND_TEST, COMMAND_TEST_OUTPUT, context))
   .catch(logger.error);
