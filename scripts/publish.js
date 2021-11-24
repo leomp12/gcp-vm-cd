561153d7549693c7912d56cc3bbbdaf295bf6ff2
Leonardo Matos <leomp120894@gmail.com>
2021-11-24 16:10:55 -0300

    feat(publish): script to publish to pubsub vms cd topic

---
+++ scripts/publish.js
@@ -0,0 +1,32 @@
+/* eslint-disable no-console */
+
+const { PubSub } = require('@google-cloud/pubsub');
+
+const {
+  PUBSUB_TOPIC,
+  COMMAND_WORKING_DIR,
+  COMMAND_PULL,
+  COMMAND_RESTART,
+  COMMAND_TEST,
+  REGEX_TEST_OUTPUT,
+} = process.env;
+
+const pubsub = new PubSub();
+const topicName = PUBSUB_TOPIC || 'vms_cd';
+const topic = pubsub.topic(topicName);
+
+const json = {
+  commandWorkingDir: COMMAND_WORKING_DIR,
+  commandPull: COMMAND_PULL,
+  commandRestart: COMMAND_RESTART,
+  commandTest: COMMAND_TEST,
+  regexTestOutput: REGEX_TEST_OUTPUT,
+};
+
+try {
+  const messageId = await topic.publishMessage({ json });
+  console.log(`Message ${messageId} published with data:\n${JSON.stringify(json, null, 2)}`);
+} catch (error) {
+  console.error(`Received error while publishing: ${error.message}`);
+  process.exitCode = 1;
+}
